5632ba728efd59a54eb6b7d1d009043e530dd3af
Anqi-Dai <adai@bu.edu>
2018-11-30 15:08:07 -0500

    The script to do sequence analysis on the RI including the GC and the MaxEnt score

---
+++ analysis/05_IRfinder/script/03_RI.analysis2.seqence.R
@@ -0,0 +1,79 @@
+# This script is to do analysis on RI locus and append information of below to the df
+# * GC content 
+# * MaxEnt score
+
+library(tidyverse)
+library(GenomicRanges)
+library(BSgenome)
+library(BSgenome.Hsapiens.NCBI.GRCh38)
+library(seqinr)
+
+
+# load the data
+RES <- read_csv('../data/IRfinder.res.filtered.14n89.analysis.csv', col_types = cols(Group = col_factor()))
+
+##################################################################################
+
+# 1. The GC content of the RI
+
+RES <- RES %>%
+  mutate(Start = Start + 1, Chr = factor(Chr))
+
+genome <- BSgenome.Hsapiens.NCBI.GRCh38
+seqlevelsStyle(genome) = "UCSC"
+grange = makeGRangesFromDataFrame(RES, seqnames.field=c("Chr"), start.field="Start",
+                                  end.field=c("End"), strand.field="Direction")
+seq = BSgenome::getSeq(genome, grange)
+
+RES <- RES %>%
+  mutate(Seq = as.character(seq))  %>%
+  mutate(GC = stringr::str_count(Seq, "G") + stringr::str_count(Seq, "C")) %>%
+  mutate(GC_per = GC / stringr::str_length(Seq) * 100)
+
+##################################################################################
+
+# 2. The score of the 5' ss and 3' ss  according to the Maximum Entropy Model
+
+# check whether the splice site is canonical ss
+
+RES <- RES %>%
+  mutate(Donor = stringr::str_sub(Seq, 1, 2),
+         Acceptor = stringr::str_sub(Seq, -2),
+         Splicesite_pair = paste(Donor, Acceptor, sep = '-')) 
+
+# * Score 5' splice site: Each sequence must be 9 bases long. 3 bases in exon and 6 bases in intron
+# * Score 3' splice site: Each sequence must be 23 bases long. 20 bases in the intron and 3 base in the exon
+
+RES <-  RES %>%
+  mutate(ss5_start = Start - 3,
+         ss5_end = Start + 5,
+         ss3_start = End -20,
+         ss3_end = End + 2 )
+
+grange5 <-  makeGRangesFromDataFrame(RES, 
+                                     seqnames.field=c("Chr"),  
+                                     start.field="ss5_start", 
+                                     end.field=c("ss5_end"), 
+                                     strand.field="Direction")
+
+
+grange3 <-  makeGRangesFromDataFrame(RES, 
+                                     seqnames.field=c("Chr"),  
+                                     start.field="ss3_start", 
+                                     end.field=c("ss3_end"), 
+                                     strand.field="Direction")
+
+RES <- RES %>%
+  mutate(ss5_seq = as.character(BSgenome::getSeq(genome, grange5)),
+         ss3_seq = as.character(BSgenome::getSeq(genome, grange3)),
+         id = paste(Chr,paste(Start, End, sep = '-'), sep = ':' ) )
+
+# write out to fasta file
+write.fasta(sequences = as.list(RES$ss5_seq), 
+            names = RES$id,
+            file.out =  '../data/ss5.seq.fasta')
+
+
+write.fasta(sequences = as.list(RES$ss3_seq), 
+            names = RES$id,
+            file.out =  '../data/ss3.seq.fasta')
