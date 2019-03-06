9cd58cb9d4ce31ccb371c2284160ddf207be1fea
Anqi-Dai <adai@bu.edu>
2019-03-06 12:10:15 -0500

    continue on the tools run

---
+++ analysis/14_Day3/analysis/06_rMATS/run.rMATS.on.bam.with.replicates.RF14_control.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#$ -P flynngrp
+#$ -cwd
+
+python rMATS_pkg/rMATS.4.0.2/rMATS-turbo-Linux-UCS4/rmats.py --b1 data/RF14.bam.txt  --b2 data/RFControl.bam.txt \
+    --gtf /usr4/bs831/adai/bubhub-home/SDE2/reference/v27/gencode.v27.annotation.gtf --od output/  -t paired \
+    --readLength  76  --cstat 0.1 --libType fr-secondstrand 
