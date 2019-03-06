d46a05ddd276d0a2000d005249031b465f153277
Anqi-Dai <adai@bu.edu>
2019-03-06 12:59:18 -0500

    now finished the three runs

--- analysis/14_Day3/analysis/06_rMATS/run.rMATS.on.bam.with.replicates.RF89_control.sh
+++ analysis/14_Day3/analysis/06_rMATS/run.rMATS.on.bam.with.replicates.RF89_control.sh
@@ -3,6 +3,6 @@
 #$ -P flynngrp
 #$ -cwd
 
-python rMATS_pkg/rMATS.4.0.2/rMATS-turbo-Linux-UCS4/rmats.py --b1 data/RF89.bam.txt  --b2 data/RFControl.bam.txt \
+python ../../../06_rMATS/rMATS_pkg/rMATS.4.0.2/rMATS-turbo-Linux-UCS4/rmats.py --b1 data/RF89.bam.txt  --b2 data/RFControl.bam.txt \
     --gtf /usr4/bs831/adai/bubhub-home/SDE2/reference/v27/gencode.v27.annotation.gtf --od output/RF89_control/  -t paired \
     --readLength  76  --cstat 0.1 --libType fr-secondstrand 
